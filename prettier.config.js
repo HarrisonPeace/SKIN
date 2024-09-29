f475772e1cc63569676efc4b57d880a811423bc3
Harrison Peace <harrison@apwines.com>
2024-09-29 16:32:09 +1000

    Setup initial project configuration (Prettier, Tailwind, favicon, SCSS, fonts)

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['tw'],
+}
